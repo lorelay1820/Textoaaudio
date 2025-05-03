483c8539d2cbf1bbea621ba0be0cd431a0dfe2a6
lorelay1820 <lorelay1820@hotmail.com>
2025-05-02 21:58:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 import base64
 
 st.title("Conversión de Texto a Audio")
-image = Image.open('gato_raton.png')
+image = Image.open('amarillo.png')
 st.image(image, width=350)
 with st.sidebar:
     st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
@@ -19,13 +19,12 @@ try:
 except:
     pass
 
-st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
-         ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
-         '  '
-         ' Franz Kafka.'
+st.subheader(" El Pájaro Amarillo y el Espejo.")
+st.write('Un pequeño pájaro amarillo volaba alegre cada mañana, admirando su reflejo en el lago.
+'Se creía el más bello del bosque y evitaba a los demás animales, pensando que no estaban a su altura.
+'Un día, vio un objeto brillante entre los arbustos: era un espejo roto.
+'Al mirarse, su reflejo se distorsionó y pensó que había perdido su belleza. Triste, bajó la cabeza y empezó a hablar con los otros animales por primera vez.
+'Para su sorpresa, ellos lo recibieron con cariño. Descubrió que su canto y su alegría eran mucho más valiosos que sus plumas doradas.'
         
         )
            
